82cf670199448733d48c09ba5551e383241987e2
J092448 <ljh0924487@gmail.com>
2025-02-25 17:21:56 +0900

    1

--- src/main/java/com/ontherocks/cocktail/config/SecurityConfig.java
+++ src/main/java/com/ontherocks/cocktail/config/SecurityConfig.java
@@ -10,11 +10,11 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/images/**", "/css/**", "/js/**", "/menu/**", "/cocktail/templates/**", "/menuOrder.html").permitAll() // 인증 없이 접근할 수 있는 리소스 추가
-//                        .anyRequest().authenticated() // 그 외 요청은 인증 필요
-//                )
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/images/**", "/css/**", "/js/**", "/menu/**", "/cocktail/templates/**", "/menuOrder.html","/csPages/**").permitAll() // 인증 없이 접근할 수 있는 리소스 추가
+                        .anyRequest().authenticated() // 그 외 요청은 인증 필요
+                );
 //                .logout(logout -> logout.permitAll()) // 로그아웃 기능 허용
         http.csrf(csrf -> csrf.disable()); // CSRF 보호 비활성화
 
